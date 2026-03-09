4f3421ffe7326bcd38940213929cfbc1fd170c95
kavish258 <Kavishonline0@gmail.com>
2026-03-10 00:36:18 +0530

    set  homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/react";
 
 const Navbar = () => {
+
+const [isOpen, setIsOpen] = useState(false)
+const {user} = useUser()
+const {openSignIn} = useClerk ()
+
+const navigate = useNavigate ()
+
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
 
@@ -11,29 +19,40 @@ const Navbar = () => {
         <img src={assets.logo} alt="logo" className="w-36 h-auto" />
       </Link>
 
-      <div className="max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-40 flex max-md:flex-col md:flex-row items-center max-md:justify-center gap-8 md:px-8 py-3 max-md:h-screen 
+      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-40 flex max-md:flex-col md:flex-row items-center max-md:justify-center gap-8 md:px-8 py-3 max-md:h-screen 
                       md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border 
-                     border-gray-300/20 overflow-hidden transition-[width] duration-300 ">
+                     border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0' }`}>
 
-        <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" />
+        <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" onClick={()=> setIsOpen(!isOpen)} />
 
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/theaters">Theaters</Link>
-        <Link to="/releases">Releases</Link>
-        <Link to="/favourite">Favourite</Link>
+        <Link onClick={()=> {scrollTo(0,0),setIsOpen(false)}} to="/">Home</Link>
+        <Link onClick={()=> {scrollTo(0,0),setIsOpen(false)}} to="/movies">Movies</Link>
+        <Link onClick={()=> {scrollTo(0,0),setIsOpen(false)}} to="/theaters">Theaters</Link>
+        <Link onClick={()=> {scrollTo(0,0),setIsOpen(false)}} to="/releases">Releases</Link>
+        <Link onClick={()=> {scrollTo(0,0),setIsOpen(false)}} to="/favourite">Favourite</Link>
 
       </div>
 
       <div className="flex items-center gap-8">
         <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
+        {
+          !user ? (
+            <button onClick={openSignIn} className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
           Login
         </button>
+          ) : (
+            <UserButton >
+              <UserButton.MenuItems>
+                <UserButton.Action label="My Bookings" labelIcon={<TicketPlus width={15} />} onClick={() => navigate('/my-booking')} />
+              </UserButton.MenuItems>
+            </UserButton>
+          )
+        }
+
+        
       </div>
 
-      <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" />
+      <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" onClick={()=> setIsOpen(!isOpen)} />
 
     </div>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/react'
+
+
+
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={publishableKey}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
   
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  
 )
