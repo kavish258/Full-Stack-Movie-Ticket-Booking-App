210e08c97d81ef7b822bb54e67bfa09862972bdd
kavish258 <Kavishonline0@gmail.com>
2026-03-10 18:38:08 +0530

    set trailerSection code

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -20,7 +20,7 @@ const FeaturedSection = () => {
           </button>
       </div>
 
-      <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
+      <div className='flex flex-wrap justify-center gap-8 mt-8'>
         {dummyShowsData.slice(0, 4).map((show) => (
          <MovieCard key={show._id} movie={show} />
          ))}
