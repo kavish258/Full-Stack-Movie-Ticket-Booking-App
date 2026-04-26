771884e0e8fe8a65b314d1ebbf46ede1268d0675
kavish258 <Kavishonline0@gmail.com>
2026-04-26 19:06:52 +0530

    code set

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -4,6 +4,7 @@ import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
 import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
 import timeFormat from '../lib/timeFormat'
+import DateSelect from '../components/DateSelect'
 
 const MovieDetails = () => {
   const {id} = useParams()
@@ -43,13 +44,27 @@ const MovieDetails = () => {
                 <PlayCircleIcon className='w-5 h-5'/>
                 Watch Trailer
                 </button>
-              <a href='' className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer acrive:scale-95'>Buy Ticket</a>
+              <a href='' className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95'>Buy Ticket</a>
               <button className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
                 <Heart className={'w-5 h-5'}/>
               </button>
-            </div>     
-          </div>
-      </div>
+            </div>
+        </div>
+    </div>
+            <p className='text-lg font-medium mt-20 '>Your Favourite Cast</p>
+            <div className='overflow-x-auto no-scrollbar mt-8 pb-4'>
+              <div className='flex items-center gap-4 w-max px-4'>
+                {show.movie.casts.slice(0,12).map((cast,index)=> (
+                  <div key={index} className='flex flex-col items-center text-center' >
+                    <img src={cast.profile_path} alt='' className='rounded-full h-20 md:h-20 aspect-square object-cover ' />
+                    <p className='font-medium text-xs mt-3'>{cast.name}</p>
+
+                  </div>
+                ))}
+              </div>
+           </div>
+           <DateSelect dateTime={show.dateTime} id={id} />
+
     </div>
   ) : <div>Loading...</div>
 }
