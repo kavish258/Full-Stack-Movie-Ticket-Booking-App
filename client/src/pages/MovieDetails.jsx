b40e2ec7231c4ca4ab209d56686dae5e0df4bbf4
kavish258 <Kavishonline0@gmail.com>
2026-04-26 16:01:34 +0530

    set movieDetails head

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
-import { StarIcon } from 'lucide-react'
+import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
+import timeFormat from '../lib/timeFormat'
 
 const MovieDetails = () => {
   const {id} = useParams()
   const [show , setShow] = useState (null)
 
   const getShow = async () => {
-    const show = dummyShowsData.find(show => show.id === id)
+    const show = dummyShowsData.find(show => show.id === Number(id))
     setShow({
       movie: show,
       dataTime: dummyDateTimeData
@@ -22,7 +23,7 @@ const MovieDetails = () => {
     <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
       <div className='flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
 
-        <img src={show.movie.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 cbject-cover' />
+        <img src={show.movie.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
 
         <div className='relative flex flex-col gap-3'>
           <BlurCircle top="-100px" left='-100px'/>
@@ -35,8 +36,18 @@ const MovieDetails = () => {
             <p className='text-gray-400 mt-2 text-sm leading-tight max-w-xl'>{show.movie.overview}</p>
 
             <p>
-              {timeFormat(show.movie.runtime)} • {show.movie.genres.map(genre => genre.name).join(",")} • {show.movie.release_date.split(",")[0]}
-            </p>       
+              {timeFormat(show.movie.runtime)} • {show.movie.genres.map(genre => genre.name).join(",")} • {show.movie.release_date}
+            </p>  
+            <div className='flex items-center flex-wrap gap-4 mt-4'>
+              <button className='flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95'>
+                <PlayCircleIcon className='w-5 h-5'/>
+                Watch Trailer
+                </button>
+              <a href='' className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer acrive:scale-95'>Buy Ticket</a>
+              <button className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
+                <Heart className={'w-5 h-5'}/>
+              </button>
+            </div>     
           </div>
       </div>
     </div>
